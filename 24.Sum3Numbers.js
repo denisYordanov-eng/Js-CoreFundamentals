182aa16922474139f7705c42d0a47097fe001eb8
denisYordanov-eng <denisivov437@gmail.com>
2025-08-12 07:05:14 +0300

    Made a task.

---
+++ 24.Sum3Numbers.js
@@ -0,0 +1,11 @@
+function sum(input){
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    let num3 = Number(input[2]);
+    function sumNumbers (num1, num2,num3) {
+        return num1 + num2 + num3;
+    }
+    let result = sumNumbers(num1, num2, num3);
+    console.log(result);
+}
+sum(['2','3','4']);
