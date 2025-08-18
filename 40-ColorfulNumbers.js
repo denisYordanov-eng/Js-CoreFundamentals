5bc2d333b6c861bd73d49b23dc0e3848141628d6
denisYordanov-eng <denisivov437@gmail.com>
2025-08-19 02:10:37 +0300

    Made a task.

---
+++ 40-ColorfulNumbers.js
@@ -0,0 +1,11 @@
+function colourfulNumbers(numbers) {
+    console.log("<ul>");
+    for (let i =1; i <= numbers; i++) {
+        if(i % 2 !==0){
+            console.log(`<li><span style='color:green'>${i}</span></li>`);
+        }else {
+            console.log(`<li><span style='color:blue'>${i}</span></li>`);
+        }
+    }
+    console.log("</ul>");
+}
