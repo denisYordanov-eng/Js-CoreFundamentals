277ce2193df6c147d76b05300c2aa11d3fec8c19
denisYordanov-eng <denisivov437@gmail.com>
2025-08-20 06:57:44 +0300

    Made a task

---
+++ 43-PrimeNumberChecker.js
@@ -0,0 +1,14 @@
+function checkIfNumericIsPrime(num) {
+    if (num <= 1){
+        return false;
+    }
+
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0){
+            return false;
+            }
+    }
+    return true;
+}
+checkIfNumericIsPrime(7);
