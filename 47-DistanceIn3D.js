b38bdd4f7e2cf02f3c0bab446ab2e7249163cfa9
denisYordanov-eng <denisivov437@gmail.com>
2025-08-23 07:17:30 +0300

    Made a task.

---
+++ 47-DistanceIn3D.js
@@ -0,0 +1,12 @@
+function calcDistance3D(input){
+    let x1 = input[0];
+    let y1 = input[1];
+    let z1 = input[2];
+
+    let x2 = input[3];
+    let y2 = input[4];
+    let z2 = input[5];
+
+    let result = Math.pow(x1-x2,2) + Math.pow(y1 - y2,2) + Math.pow(z1-z2,2);
+    console.log(Math.sqrt(result));
+}
