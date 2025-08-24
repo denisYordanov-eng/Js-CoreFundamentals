94110b59b8aa69761e31998d4deb42c59db1fe11
denisYordanov-eng <denisivov437@gmail.com>
2025-08-24 07:15:10 +0300

    Made a task.

---
+++ 52-NowPlaying.js
@@ -0,0 +1,8 @@
+function findTheCurrentSong(arr){
+    let songName = arr[0];
+    let singer = arr[1];
+    let songLength = arr[2];
+
+    //Now Playing: Nelly – Number One [4:09]
+    console.log(`Now Playing: ${singer} - ${songName} [${songLength}]`);
+}
