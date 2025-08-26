8504dbbfccaf84cfd674f14577ee6d8f49578b83
denisYordanov-eng <denisivov437@gmail.com>
2025-08-26 07:20:29 +0300

    Made a task.

---
+++ 58-PointInRectangle.js
@@ -0,0 +1,14 @@
+function pointInRectangle(arr) {
+    let x = arr[0];
+    let y = arr[1];
+    let xMin = arr[2];
+    let xMax = arr[3];
+    let yMin = arr[4];
+    let yMax = arr[5];
+
+        if(x >= xMin && x <= xMax && y >= yMin && y <= yMax) {
+          return  console.log("inside")
+        }else{
+          return  console.log("outside")
+        }
+}
