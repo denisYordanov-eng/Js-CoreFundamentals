f54373e1c44337f2a750054bb961f20d87cbbd9b
denisYordanov-eng <denisivov437@gmail.com>
2025-08-26 07:25:39 +0300

    Made a task.

---
+++ 59-OddNumbersOneToN.js
@@ -0,0 +1,11 @@
+function findOddNumbers(n) {
+    // for (let i = 1; i <= n; i++) {
+    //     if (i % 2 !== 0) {
+    //         console.log(i);
+    //     }
+    // }
+
+    for (let i = 1; i <= n; i += 2) {
+        console.log(i);
+    }
+}
