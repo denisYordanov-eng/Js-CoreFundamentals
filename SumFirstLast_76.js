86968eb6baf2fd69a0fccd3b86dc7458fb529b10
denisYordanov-eng <denisivov437@gmail.com>
2025-12-20 17:43:49 +0200

    Made a task.

---
+++ SumFirstLast_76.js
@@ -0,0 +1,12 @@
+
+function sumFirstAndLastElement(arr) {
+
+    let sum = Number(arr[0]) + Number(arr[arr.length - 1]);
+    console.log(sum);
+}
+
+sumFirstAndLastElement(['20', '30', '40']);
+
+
+
+
